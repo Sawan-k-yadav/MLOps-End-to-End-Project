d4612b79e87fd5fe372266b79cdc9cd9691b5916
Sawan-k-yadav <sawan.gomia@gmail.com>
2024-09-10 05:41:27 +0530

    Added data component files

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.exception.exception import customException
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class DataIngestionConfig:
+    """Configuration of raw, training and testing data in artifact folder"""
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+
+        try:
+            data=pd.read_csv(Path(os.path.join("./experiment","gemstone.csv")))
+            logging.info(" I have read dataset as a df")
+
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
+            
+            logging.info("Starting to perform train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured during Data ingestion stage")
+            raise customException(e, sys)
+
+
+if __name__=="__main__":
+    obj=DataIngestion()
+
+    obj.initiate_data_ingestion()
---
+++ src/components/model_evaluation.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pickle
+from src.utils.utils import load_object
+from dataclasses import dataclass
+from src.logger.logging import logging
+from src.exception.exception import customException
+
+@dataclass
+class ModelEvaluationConfig:
+    pass
+
+class ModelEvaluation:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customException(e, sys)
+
